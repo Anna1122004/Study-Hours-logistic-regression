511e5173ce2f8732621dcf25bcaa9a4440c5ebeb
Anna1122004 <mathewt9747592371@gmail.com>
2026-09-17 11:29:42 +0530

    Update Study_hours_logistic_regression.py

--- Study_hours_logistic_regression.py
+++ Study_hours_logistic_regression.py
@@ -8,9 +8,16 @@ hours = st.number_input(
     max_value=15.0,
     value=5.0
 )
+attendance=st.number_input(
+    "Enter attendance",
+    min_value=0.0,
+    max_value=100.0,
+    value=50.0
+)
+
 if st.button("Predict"):
-    prediction = model.predict([[hours]])
-    probability = model.predict_proba([[hours]])
+    prediction = model.predict([[hours,attendance]])
+    probability = model.predict_proba([[hours,attendance]])
     pass_probability = probability[0][1] * 100
     fail_probability = probability[0][0] * 100
     if prediction[0] == 1:
