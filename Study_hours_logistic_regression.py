6b6bfc724416f6cf5cd271623889ecf09cd2e99b
Anna1122004 <mathewt9747592371@gmail.com>
2026-09-17 09:35:13 +0530

    Add prediction feedback for student pass/fail

--- Study_hours_logistic_regression.py
+++ Study_hours_logistic_regression.py
@@ -5,3 +5,7 @@ st.title("Student Pass / Fail based on Study Hours")
 hours=st.number_input("Enter study hours",min_value=0.0,max_value=15.0,value=5.0)
 if st.button("Predict "):
    prediction=model.predict([[hours]])
+   if prediction[0]==1:
+      st.success("Student will PASS")
+   else:
+      st.error("Student will FAIL")
