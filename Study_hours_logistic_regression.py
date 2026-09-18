e6e1b02463b94ce91203351b0c1b344720db4b59
Anna1122004 <mathewt9747592371@gmail.com>
2026-09-18 12:29:41 +0530

    Update Study_hours_logistic_regression.py

--- Study_hours_logistic_regression.py
+++ Study_hours_logistic_regression.py
@@ -1,4 +1,4 @@
-```python
+
 import gradio as gr
 import joblib
 import pandas as pd
@@ -60,4 +60,4 @@ if __name__ == "__main__":
         server_name="0.0.0.0",
         server_port=int(os.environ.get("PORT", 7860))
     )
-```
+
